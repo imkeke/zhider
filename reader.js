161e1f13de8776cf2cde0334b59fc5097695e072
keke <zhengkefeng15@gmail.com>
2015-04-03 16:34:26 +0800

    retab

--- reader.js
+++ reader.js
@@ -2,38 +2,38 @@ var $readerBtn = $('<div class="zm-reader-btn">阅读</div>')
   , $page = $('<div class="pager"><div class="page"></div></div>')
 
 $('body').on('click', '.zm-reader-btn', function() {
-    var e = $(this)
-      , $title = $('<h1></h1>').text(e.parents('.content').find('h2').find('a').text())
-      , $more = e.parent().find('.zh-summary')
-      , content = $more.length ?
-                  e.parent().find('.zm-editable-content').html() :
-                  ''
+  var e = $(this)
+    , $title = $('<h1></h1>').text(e.parents('.content').find('h2').find('a').text())
+    , $more = e.parent().find('.zh-summary')
+    , content = $more.length ?
+                e.parent().find('.zm-editable-content').html() :
+                ''
 
-    $more.trigger('click')
-    e.parents('.content').find('.collapse')[0].click()
+  $more.trigger('click')
+  e.parents('.content').find('.collapse')[0].click()
 
-    content = e.parent().find('.zm-editable-content').html()
+  content = e.parent().find('.zm-editable-content').html()
 
-    $page.find('.page').html(content).prepend($title)
+  $page.find('.page').html(content).prepend($title)
 
-    $page.show()
+  $page.show()
 
-    $('body').addClass('bodyon')
+  $('body').addClass('bodyon')
 })
 
 $page.on('click', function(event) {
-    var e = $(this)
+  var e = $(this)
 
-    if ($(event.target).is('.pager')) {
-        e.find('.page').empty()
-        e.hide()
-        $('body').removeClass('bodyon')
-    }
+  if ($(event.target).is('.pager')) {
+    e.find('.page').empty()
+    e.hide()
+    $('body').removeClass('bodyon')
+  }
 })
 
 function setBtn() {
-    $('.zm-votebar:not(.zm-votebar-already)')
-    .after($readerBtn).addClass('zm-votebar-already')
+  $('.zm-votebar:not(.zm-votebar-already)')
+  .after($readerBtn).addClass('zm-votebar-already')
 }
 setBtn()
 
@@ -41,11 +41,11 @@ var defaultFeedLength = $('#js-home-feed-list .feed-item').length
   , feedLength
 
 setInterval(function() {
-    feedLength = $('#js-home-feed-list .feed-item').length
-    if (feedLength > defaultFeedLength) {
-        setBtn()
-        defaultFeedLength = feedLength
-    }
+  feedLength = $('#js-home-feed-list .feed-item').length
+  if (feedLength > defaultFeedLength) {
+      setBtn()
+      defaultFeedLength = feedLength
+  }
 }, 1000)
 
 $('body').append($page)
